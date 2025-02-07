7ddfa1743d654ca4b928a82bb73e02052ed61ffb
jcsusbilla <jcsusbilla@gmail.com>
2025-02-06 20:19:32 -0800

    Fix directory issue

--- JobFinder/app/index.js
+++ JobFinder/app/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Text, View, TouchableOpacity, Image } from "react-native";
 import { useRouter } from "expo-router";
-// import appStyles from "C:/Users/jc/Documents/Project1_JC/JobFinder/app/styles/appStyles.js";
 import appStyles from "./styles/appStyles.js";
 
 export default function LandingScreen() {
